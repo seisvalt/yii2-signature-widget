5609d1834b6d526b824dae6db20bafb6a6bab206
seisvalt <seisvalt@users.noreply.github.com>
2023-02-27 14:56:43 -0500

    fixed bug socket

--- assets/app.js
+++ assets/app.js
@@ -48,7 +48,7 @@ function sendToUrl(dataURL,url) {
         $.post(url, {signature: base64data, token: token}, function (data) {
 
             if (JSON.parse(data).message == true) {
-                socket.emit('setDialMessage', {value: token, step: 3});
+                //socket.emit('setDialMessage', {value: token, step: 3});
                 window.location.replace(urlCallback);
             }
         });
